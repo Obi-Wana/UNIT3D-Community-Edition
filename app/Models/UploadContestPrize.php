HDInnovations/UNIT3D

Add upload contest events
Add a modular base set-up that allows adding more different event types.
This one adds upload contests. More can be added the same way with minimal coding efforts.

Implementing a completely automated system may not be feasible, as there are too many individual variations and nuances to account for.

app/Models/UploadContestPrize.php
@@ -1,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     Obi-wana
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use AllowDynamicProperties;
+
+/**
+ * App\Models\UploadContestPrize.
+ *
+ * @property int                             $id
+ * @property int                             $upload_contest_id
+ * @property string                          $type
+ * @property int                             $amount
+ * @property int                             $position
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
+#[AllowDynamicProperties]
+final class UploadContestPrize extends Model
+{
+    /** @use HasFactory<\Database\Factories\UploadContestPrizeFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var string[]
+     */
+    protected $guarded = [];
+
+    /**
+     * Get the event that owns the prize.
+     *
+     * @return BelongsTo<Event, $this>
+     */
+    public function contest(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+}

Obi-Wana/UNIT3D-Community-Edition

app/Models/UploadContestPrize.php
@@ -1,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     Obi-wana
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use AllowDynamicProperties;
+
+/**
+ * App\Models\UploadContestPrize.
+ *
+ * @property int                             $id
+ * @property int                             $upload_contest_id
+ * @property string                          $type
+ * @property int                             $amount
+ * @property int                             $position
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
+#[AllowDynamicProperties]
+final class UploadContestPrize extends Model
+{
+    /** @use HasFactory<\Database\Factories\UploadContestPrizeFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var string[]
+     */
+    protected $guarded = [];
+
+    /**
+     * Get the UploadContest that owns the prize.
+     *
+     * @return BelongsTo<UploadContest, $this>
+     */
+    public function contest(): BelongsTo
+    {
+        return $this->belongsTo(UploadContest::class);
+    }
+}